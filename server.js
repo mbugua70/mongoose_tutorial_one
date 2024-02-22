8f015eeced70575376b495249d6edc1dc9eaa0f7
john mbugua <johnymbugua1997@gmail.com>
2024-02-22 10:45:29 +0300

    additional changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db =
 mongoose.connect(db);
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h1>Hello John Doe</h1>");
 });
@@ -34,7 +34,46 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
+// app.post("/books", async (req, res) => {
+//   try {
+//     const newBook = new Book();
+//     newBook.title = req.body.title;
+//     newBook.author = req.body.author;
+//     newBook.category = req.body.category;
+//     const book = await newBook.save();
+//     res.status(200).json({ success: true, msg: book });
+//   } catch (err) {
+//     res.status(500).json({ success: false, msg: `An Error occured ${err}` });
+//   }
+// });
 
+// below shows the use of create method
+
+app.post("/books", async (req, res) => {
+  try {
+    const result = await Book.create(req.body);
+    res.status(200).json({ success: true, msg: result });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: `An error occured ${err}` });
+  }
+});
+
+// update
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const newUpdate = await Book.findOneAndUpdate(
+      { _id },
+      { $set: { title: req.body.title } },
+      { upsert: true }
+    );
+
+    res.status(204).json({ success: true, data: newUpdate });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: `An Error Occurred ${err}` });
+  }
+});
 
 
 
